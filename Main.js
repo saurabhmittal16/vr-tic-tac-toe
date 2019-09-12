bb6ef6f8b5da9da7b92f1967180807fdee007f7f
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-09-13 00:21:11 +0530

    Update grid component

--- Main.js
+++ Main.js
@@ -1,61 +1,55 @@
-import * as React from "react";
+import React from "react";
 import { StyleSheet, Text, View, VrButton } from "react-360";
 
-const CurrentPost = props => {
-	return (
-		<View style={styles.wrapper}>
-			<Text style={styles.title}>Tic Tac Toe</Text>
-			<View style={styles.grid}>
-				<View style={styles.row}>
-					<VrButton style={[styles.box, styles.right, styles.bottom]}>
-						<Text> </Text>
-					</VrButton>
-					<VrButton
-						style={[styles.box, styles.left, styles.right, styles.bottom]}
-					>
-						<Text> </Text>
-					</VrButton>
-					<VrButton style={[styles.box, styles.left, styles.bottom]}>
-						<Text> </Text>
-					</VrButton>
-				</View>
-				<View style={styles.row}>
-					<VrButton
-						style={[styles.box, styles.right, styles.top, styles.bottom]}
-					>
-						<Text> </Text>
-					</VrButton>
-					<VrButton
-						style={[
-							styles.box,
-							styles.left,
-							styles.right,
-							styles.top,
-							styles.bottom
-						]}
-					>
-						<Text> </Text>
-					</VrButton>
-					<VrButton
-						style={[styles.box, styles.left, styles.top, styles.bottom]}
-					>
-						<Text> </Text>
-					</VrButton>
-				</View>
-				<View style={styles.row}>
-					<VrButton style={[styles.box, styles.right, styles.top]}>
-						<Text> </Text>
-					</VrButton>
-					<VrButton style={[styles.box, styles.left, styles.right, styles.top]}>
-						<Text> </Text>
-					</VrButton>
-					<VrButton style={[styles.box, styles.left, styles.top]}>
-						<Text> </Text>
-					</VrButton>
+class Main extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			val: ["X", "O", "X", "O", "X", "X", "X", "X", "O"]
+		};
+	}
+	render() {
+		return (
+			<View style={styles.wrapper}>
+				<Text style={styles.title}>Tic Tac Toe</Text>
+				<View style={styles.grid}>
+					{[0, 1, 2].map(j => (
+						<View style={styles.row}>
+							{[3 * j, 3 * j + 1, 3 * j + 2].map(i => (
+								// i -> index of the cell in grid (0, 9)
+								<VrButton style={getStyles(i)}>
+									<Text style={styles.item}>{this.state.val[i]}</Text>
+								</VrButton>
+							))}
+						</View>
+					))}
 				</View>
 			</View>
-		</View>
-	);
+		);
+	}
+}
+
+const getStyles = index => {
+	switch (index) {
+		case 0:
+			return [styles.box, styles.right, styles.bottom];
+		case 1:
+			return [styles.box, styles.left, styles.right, styles.bottom];
+		case 2:
+			return [styles.box, styles.left, styles.bottom];
+		case 3:
+			return [styles.box, styles.right, styles.top, styles.bottom];
+		case 4:
+			return [styles.box, styles.left, styles.right, styles.top, styles.bottom];
+		case 5:
+			return [styles.box, styles.left, styles.top, styles.bottom];
+		case 6:
+			return [styles.box, styles.right, styles.top];
+		case 7:
+			return [styles.box, styles.left, styles.right, styles.top];
+		case 8:
+			return [styles.box, styles.left, styles.top];
+	}
 };
 
 const styles = StyleSheet.create({
@@ -88,6 +82,12 @@ const styles = StyleSheet.create({
 		height: 112,
 		borderColor: "white"
 	},
+	item: {
+		fontSize: 72,
+		fontWeight: "700",
+		textAlign: "center",
+		color: "white"
+	},
 	left: {
 		borderLeftWidth: 2
 	},
@@ -102,4 +102,4 @@ const styles = StyleSheet.create({
 	}
 });
 
-export default CurrentPost;
+export default Main;
